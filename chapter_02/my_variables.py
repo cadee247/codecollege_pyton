3d0b7d7b667b8767d428661e1db3054d0c1b5606
cadee247 <rousseaucadee@gmail.com>
2025-02-24 14:11:00 +0200

    python

---
+++ chapter_02/my_variables.py
@@ -0,0 +1,37 @@
+name = "Cadee"
+
+surname = "Rousseau"
+
+print(name +" "+ surname)
+
+my_sentence = "Cadee joined the python course"
+print(my_sentence.title())
+
+my_initials = f"{name} {surname}"
+print(my_initials)
+
+print(my_sentence.title())
+name = name 
+print(name)
+print(f"aftenoon, " + name + "! " + my_sentence.title())
+
+
+
+
+print("i went to the store. i like to listen to music. My mom is awesome.")
+print("\ti like cooking and baking with my bestfriend on weekends.\ni like watching movies with my mom on friday nights.")
+
+
+print("         cadee".rstrip())
+
+my_link = "https://www.google.com"
+print(my_link.removeprefix("https://www."))
+
+animal, food, plant = 10, 20, 30
+print(animal)
+print(food)
+print(plant)
+
+import this 
+# The Zen of Python, by Tim Peters
+# Beautiful is better than ugly.
